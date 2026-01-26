090092605ae04ebb7f1d1d0c6791e7e57ab4d8b0
Gavin-Shi <gshi4three@gmail.com>
2026-01-26 17:44:32 -0500

    Completing Homework 1

---
+++ homework1/mymultlibtest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "mymultlib.h"
+
+int main()
+{
+    printf("%i\n", multIntegers(1, 2));
+    printf("%f\n", multFloats(2.24, 2.35));
+    printf("%f\n", multDoubles(4.55, 1.55));
+
+    return 0;
+}
